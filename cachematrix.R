29711532200b68af5a8bbb8a1ab1c6015b38f550
rpgitfr <rpgit@yahoo.com>
2015-07-26 20:39:16 +0200

    New commit

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,42 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
+#### Initialisation
+
+       inversematrix <- NULL
+
+#### Function to set the value of the matrix
+
+       set <- function(y) {
+
+               x <<- y
+
+               inversematrix <<- NULL
+
+       }
+
+#### Function to get the value matrix
+
+       get <- function() x
+
+#### Function to set  the inverse of the matrix
+
+       setinverse <- function(inv) inversematrix <<- inv
+
+#### Function to get the inverse of the matrix
+
+       getinverse <- function() inversematrix
+
+#### Output 
+
+       list(set = set, get = get,
+
+               setinverse = setinverse,
+
+               getinverse = getinverse)
+
+
 }
 
 
@@ -12,4 +48,31 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+#### Getting the inverse matrix
+
+       inversematrix <- x$getinverse()
+
+#### Function to get the value matrix
+
+       if(!is.null(inversematrix)) {
+
+              message("getting cached data")
+
+              return(inversematrix)
+
+       }
+
+#### Compute the inverse matrix if not calculated before
+
+       data <- x$get()
+
+       inversematrix <- solve(data)
+
+       x$setinverse(inversematrix)
+
+#### Result
+
+       inversematrix
+
 }
